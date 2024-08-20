9ba43637071db1c26e9654b360e5d619fa94af17
MohammedMaisoon <159471745+MohammedMaisoon@users.noreply.github.com>
2024-08-20 20:46:03 +0530

    Update Main.java

--- Method_Overriding/src/Main.java
+++ Method_Overriding/src/Main.java
@@ -1,17 +1,17 @@
-class Animals {
+class Animal {
     public void makeSound() {
         System.out.println("The animal makes a sound");
     }
 }
 
-class Dog extends Animals {
+class Dog extends Animal {
     @Override
     public void makeSound() {
         System.out.println("Barks");
     }
 }
 
-class Cat extends Animals {
+class Cat extends Animal {
     @Override
     public void makeSound() {
         System.out.println("Meow Meow");
@@ -20,9 +20,9 @@ class Cat extends Animals {
 
 public class Main {
     public static void main(String[] args) {
-        Animals animal = new Animals();
-        Animals dog = new Dog();
-        Animals cat = new Cat();
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
 
         animal.makeSound();
         dog.makeSound();
